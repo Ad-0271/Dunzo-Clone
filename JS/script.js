c2c84e0395fba2554effc9499ad8d437bcea89e1
Dinesh Kumar <62261026+dineshcyber@users.noreply.github.com>
2021-11-05 16:55:57 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -7,26 +7,28 @@ let showcitiespune = [
     {namepune :`Pimple Saudagar`},{namepune :`Pune Cantonment`},{namepune :`Range Hills`},{namepune :`Rasta Peth`},{namepune :`Sangamvadi`},{namepune :`Sasane Nagar`},{namepune :`Shivaji Nagar`},{namepune :`Sus`},{namepune :`Swargate`},{namepune :`Tathawade`},{namepune :`Vadgaon Sheri`},{namepune :`Viman nagar`},{namepune :`Wagholi`},{namepune :`Wagholi Extension 1`},{namepune :`Wakad`},{namepune :`Wakdeqadi`},{namepune :`Wanowrie`},{namepune :`Warje`},{namepune :`Yerwala`},{namepune :`Yewalendi`},
 ]
 
-var citypune = document.getElementById("list_state_pune");
+var city = document.getElementById("list_state_pune");
 function displaypune(){
     showcitiespune.forEach(function (el){
-        let divpune = document.createElement('div');
-        divpune.classList.add("state_list_inner");
+        let div = document.createElement('div');
+        div.classList.add("state_list_inner");
 
-        let linkpune = document.createElement('a');
-        linkpune.href = "#";
+        let link = document.createElement('a');
+        link.href = "#";
 
-        let city_para_pune = document.createElement('p');
-        city_para_pune.textContent = el.namepune;
+        let city_para = document.createElement('p');
+        city_para.textContent = el.namepune;
 
-        linkpune.append(city_para_pune);
-        divpune.append(linkpune);
-        citypune.append(divpune);
+        link.append(city_para);
+        div.append(link);
+        city.append(div);
     });
 }
 displaypune();
 
-// // Mumbai
+
+
+
 
 // // Banglore
 
